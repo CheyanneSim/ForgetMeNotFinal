71ce5dd7dcaaf0356b96d489d8c89f916c76c70d
aarushisingh1 <singh.aarushi@outlook.com>
2019-06-28 16:20:30 +0800

    barcode scanner code added, not fully functional yet

--- app/src/main/java/com/example/forgetMeNot/Inventory/AddToInventory.java
+++ app/src/main/java/com/example/forgetMeNot/Inventory/AddToInventory.java
@@ -4,14 +4,17 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.forgetMeNot.R;
+import com.example.forgetMeNot.barcodeScanner.mainBarcodeAct;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,46 +26,67 @@ public class AddToInventory extends AppCompatDialogFragment {
     private EditText expiry;
     private DialogListener listener;
     private SimpleDateFormat formatter;
+    Button scannerBtn;
+    //boolean clicked = false;
 
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        formatter = new SimpleDateFormat("dd/MM/yyyy");
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        scannerBtn = view.findViewById(R.id.scanButton);
+    }
 
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.add_to_inventory_dialog, null);
+    @Override
+    public  Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        builder.setView(view)
-                .setTitle("Add To Inventory")
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        scannerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    }
-                })
-                .setPositiveButton("Add Food", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String toAdd = food.getText().toString();
-                        try {
-                            if (toAdd.equals("")) {
-                                Toast.makeText(getContext(), "Please enter your food item!", Toast.LENGTH_LONG).show();
-                            } else if (expiry.getText().toString().equals("")) {
-                                Toast.makeText(getContext(), "Please enter the expiry date!", Toast.LENGTH_LONG).show();
-                            } else {
-                                Date date = formatter.parse(expiry.getText().toString());
-                                listener.addItem(toAdd, date);
-                            }
-                        } catch (ParseException e) {
-                            Toast.makeText(getContext(), "Please enter the expiry in the DD/MM/YYYY format!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getContext(), mainBarcodeAct.class);
+
+                startActivityForResult(intent, 0);
+            }
+        });
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            View view = inflater.inflate(R.layout.add_to_inventory_dialog, null);
+
+            builder.setView(view)
+                    .setTitle("Add To Inventory")
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
                         }
-                    }
-                });
+                    })
+                    .setPositiveButton("Add Food", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String toAdd = food.getText().toString();
+                            try {
+                                if (toAdd.equals("")) {
+                                    Toast.makeText(getContext(), "Please enter your food item!", Toast.LENGTH_LONG).show();
+                                } else if (expiry.getText().toString().equals("")) {
+                                    Toast.makeText(getContext(), "Please enter the expiry date!", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Date date = formatter.parse(expiry.getText().toString());
+                                    listener.addItem(toAdd, date);
+                                }
+                            } catch (ParseException e) {
+                                Toast.makeText(getContext(), "Please enter the expiry in the DD/MM/YYYY format!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
 
-        food = view.findViewById(R.id.non_essential_item_editText);
-        expiry = view.findViewById(R.id.expiry_editText);
-        return builder.create();
+
+            food = view.findViewById(R.id.non_essential_item_editText);
+            expiry = view.findViewById(R.id.expiry_editText);
+            return builder.create();
+
     }
 
     @Override
